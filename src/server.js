9b0241d27ed05f40fecde99faa2936ec9d21938e
marunahakhun <marina.gahun@gmail.com>
2024-06-23 11:22:35 +0200

    upd

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import pino from "pino-http"
+const app = express()
+
+
+const setupServer = () => {
+     const logger = pino({
+        transport: {
+            target: "pino-pretty"
+        }
+    })
+    app.use(cors());
+    app.use(logger);
+   
+app.get("/", (req, res)=>{
+    res.send('<h1>home page<h1/>')
+});
+    app.use((req, res) => {
+        res.status(404).json({
+            message: "Page not Found"
+        })
+    })
+app.listen(3000, () => console.log("connection with port 3000"));
+    
+}
+export {setupServer}
